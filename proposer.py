0697208befb4e1bee94c77a48a703a0e95a7138a
Jannik Luhn <jannik.luhn@brainbot.com>
2018-03-23 13:41:48 +0800

    Propose for specific shards only

--- proposer.py
+++ proposer.py
@@ -7,6 +7,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 Proposal = namedtuple("Proposal", [
+    "shard_id",
     "proposer",
     "number",
     "period",
@@ -14,6 +15,7 @@ Proposal = namedtuple("Proposal", [
 
 
 Collation = namedtuple("Collation", [
+    "shard_id",
     "number",
     "period",
 ])
@@ -21,13 +23,16 @@ Collation = namedtuple("Collation", [
 
 class SMC:
 
-    def __init__(self):
-        genesis_header = Proposal(None, 0, 0)
-        self.collation_headers = [genesis_header]
+    def __init__(self, num_shards):
+        self.num_shards = num_shards
+        self.collation_headers = {
+            shard_id: [Proposal(shard_id, None, 0, 0)]
+            for shard_id in range(self.num_shards)
+        }
         self.period = 1
 
 
-async def proposer(address, messages_out, smc):
+async def proposer(shard_id, address, messages_out, smc):
     logger = logging.getLogger("proposer")
 
     my_proposal = None
@@ -36,7 +41,7 @@ async def proposer(address, messages_out, smc):
     while True:
         await asyncio.sleep(1)
 
-        current_collation_header = smc.collation_headers[-1]
+        current_collation_header = smc.collation_headers[shard_id][-1]
         if current_collation_header != last_collation_header:
             # if my last proposal got accepted reveal the corresponding collation
             if current_collation_header.proposer == address:
@@ -46,9 +51,19 @@ async def proposer(address, messages_out, smc):
                 await messages_out.put(("revealBody", my_collation))
 
             # publish proposal for next collation
-            my_proposal = Proposal(address, current_collation_header.number + 1, smc.period)
-            my_collation = Collation(my_proposal.number, my_proposal.period)
-            logger.info("proposing for period {} with number {}".format(
+            my_proposal = Proposal(
+                shard_id,
+                address,
+                current_collation_header.number + 1,
+                smc.period
+            )
+            my_collation = Collation(
+                my_proposal.shard_id,
+                my_proposal.number,
+                my_proposal.period
+            )
+            logger.info("proposing (shard {}, period {}, number {})".format(
+                my_proposal.shard_id,
                 my_proposal.number,
                 my_proposal.period
             ))
@@ -56,10 +71,11 @@ async def proposer(address, messages_out, smc):
         last_collation_header = current_collation_header
 
 
-async def collator(messages_in, smc):
+async def collator(shard_id, messages_in, smc):
     logger = logging.getLogger("collator")
 
     while True:
+        period = smc.period
         message = await messages_in.get()
         if message[0] == "newProposal":
             proposal = message[1]
@@ -67,16 +83,16 @@ async def collator(messages_in, smc):
                 proposal.period,
                 proposal.proposer
             ))
-            smc.collation_headers.append(proposal)
+            smc.collation_headers[shard_id].append(proposal)
             smc.period += 1
 
 
 address = 0
 messages = asyncio.Queue()
-smc = SMC()
+smc = SMC(1)
 
 
 loop = asyncio.get_event_loop()
-loop.create_task(collator(messages, smc))
-loop.create_task(proposer(address, messages, smc))
+loop.create_task(collator(0, messages, smc))
+loop.create_task(proposer(0, address, messages, smc))
 loop.run_forever()
